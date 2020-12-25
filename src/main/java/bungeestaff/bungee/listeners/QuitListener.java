bbd0bbd4068ca4148368b6d7d231caf3e70090f2
Wertik <jregvx@gmail.com>
2020-12-25 19:26:37 +0100

    Fix QuitListener NPE.

--- src/main/java/bungeestaff/bungee/listeners/QuitListener.java
+++ src/main/java/bungeestaff/bungee/listeners/QuitListener.java
@@ -3,6 +3,7 @@ package bungeestaff.bungee.listeners;
 import bungeestaff.bungee.BungeeStaffPlugin;
 import bungeestaff.bungee.rabbit.MessageType;
 import bungeestaff.bungee.system.staff.StaffUser;
+import bungeestaff.bungee.util.ParseUtil;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.PlayerDisconnectEvent;
 import net.md_5.bungee.event.EventHandler;
@@ -17,13 +18,15 @@ public class QuitListener extends EventListener {
     public void onQuit(PlayerDisconnectEvent event) {
         ProxiedPlayer player = event.getPlayer();
 
+        plugin.getMessagingService().sendUserUpdate();
+
         StaffUser user = plugin.getStaffManager().getUser(player);
 
         if (user == null)
             return;
 
         plugin.getStaffManager().sendMessage(plugin.getMessage("Staff-Messages.Staff-Leave")
-                        .replace("%server_from%", player.getServer().getInfo().getName())
+                        .replace("%server_from%", ParseUtil.getOr(() -> plugin.getUserCache().getServer(player.getName()), "void"))
                         .replace("%player%", player.getName())
                         .replace("%prefix%", plugin.getPrefix(player)),
                 MessageType.STAFF_MESSAGE);
